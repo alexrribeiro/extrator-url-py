32dad76e1f08792f0a741684d6a0f6081688ea94
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-09-26 21:00:40 -0300

    A2 Parcial

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+# print(url)
 
-url_base = url[0:19]
-print(url_base)
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+# print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+parametro_busca = "moedaDestino"
+indice_parametro = url_parametros.find(parametro_busca)
+# print(indice_parametro)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
