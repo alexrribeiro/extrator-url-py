10facc6ccdee470c3cbe46e26a1017e35197db2f
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-09-28 23:13:02 -0300

    A3 Final

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
-# print(url)
+# url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+url = " "
+
+# Sanitização da URL
+url = url.strip()
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia.")
 
 # Separa base e parâmetros
 indice_interrogacao = url.find('?')
@@ -9,7 +15,7 @@ url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
 # Busca valor de um parâmetro
-parametro_busca = "moedaDestino"
+parametro_busca = "quantidade"
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
 indice_e_comercial = url_parametros.find('&', indice_valor)
