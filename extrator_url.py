2b2a1a49ed990189b344a2882df0f869652f1591
Alex R Ribeiro <alexramosribeiro@gmail.com>
2023-10-04 23:21:44 -0300

    A5 Final

--- extrator_url.py
+++ extrator_url.py
@@ -49,10 +49,67 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other): #other: objeto a direita da comparação
+        return self.url == other.url
+
+    def converte_moedas(self):
+        origem = self.get_valor_parametro("moedaOrigem")
+        destino = self.get_valor_parametro("moedaDestino")
+        quantidade = self.get_valor_parametro("quantidade")
+        VALOR_DOLAR = 5.50
+
+        print("Origem: " + origem)
+        print("Destino: " + destino)
+        print("Valor a converter: " + quantidade)
+        print(f"Cotação: {VALOR_DOLAR:.2f}")
+
+        if (destino == 'real'):
+            valor = float(quantidade) * VALOR_DOLAR
+        elif (destino == 'dolar'):
+            valor = float(quantidade) / VALOR_DOLAR
+        else:
+            valor = 0
+            print("Moeda inválida")
+
+        return valor
 
 
-
-extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real")
+# extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real")
+# extrator_url_2 = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real")
 # extrator_url = ExtratorURL(None)
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+# valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+# print(valor_quantidade)
+
+# url = "bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+# print(len(url)) # url.__len__()
+# print("O tamanho da URL:", len(extrator_url))
+# print(extrator_url)
+
+# print(extrator_url == extrator_url_2)
+# print(id(extrator_url))
+# print(id(extrator_url_2))
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real"
+url2 = "bytebank.com/cambio?quantidade=500&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+extrator_url2 = ExtratorURL(url2)
+
+# VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+# moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+# moeda_origem2 = extrator_url2.get_valor_parametro("moedaOrigem")
+# moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+# moeda_destino2 = extrator_url2.get_valor_parametro("moedaDestino")
+# quantidade = extrator_url.get_valor_parametro("quantidade")
+# quantidade2 = extrator_url2.get_valor_parametro("quantidade")
+
+# print(f'Origem: {moeda_origem}, Destino: {moeda_destino}, Quantidade: {quantidade}')
+print(f'Valor convertido: {extrator_url.converte_moedas():.2f}\n')
+
+# print(f'Origem: {moeda_origem2}, Destino: {moeda_destino2}, Quantidade: {quantidade2}')
+print(f'Valor convertido: {extrator_url2.converte_moedas():.2f}\n')
